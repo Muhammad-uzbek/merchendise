5358117838c3951cd99bce78778bf26ebbef4194
Muhammadqodir Abduraxmanov <60865598+Muhammad-uzbek@users.noreply.github.com>
2022-10-10 15:39:29 +0500

    Update checking.js

--- pages/api/checking.js
+++ pages/api/checking.js
@@ -16,6 +16,7 @@ export default async function handler(req, res) {
         return res.status( 200 ).json(result);
     }
     else {
+        console.log(req);
         return res.status( 200 ).json({"status": "not logging"});
     }
   }
