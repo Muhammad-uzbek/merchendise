e5ae32e02c126a1148b7d1e40902399e89de54ba
muhammad-uzbek <uzbekjon2003@gmail.com>
2022-10-17 20:17:43 +0500

    camera upd4

--- pages/camera/[slug].jsx
+++ pages/camera/[slug].jsx
@@ -1,11 +1,13 @@
 import { useRouter } from "next/router";
 import Webcam from "react-webcam";
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 const Post = () => {
     const router = useRouter()
     const { slug } = router.query
     const webcamRef = React.useRef(null);
     const [imgSrc, setImgSrc] = React.useState(null);
+
+    const [user, setUser] = useState(null);
     const videoConstraints = {
         facingMode: { exact: "environment" },
         width: 1080,
@@ -15,7 +17,15 @@ const Post = () => {
         const imageSrc = webcamRef.current.getScreenshot();
         setImgSrc(imageSrc);
     }, [webcamRef, setImgSrc]);
-    
+    useEffect(()=>{
+        fetch("/api/merch")
+        .then(resp => resp.json())
+        .then(response => {
+            response.map(item =>{
+                if(slug == item.userId) setUser(item)
+            })
+        });
+    },[])
     return (
         <main className="cover">
             <Webcam
@@ -26,8 +36,6 @@ const Post = () => {
                     className="camera"
             />  
             <div className="cont">
-                   
-                
                 <div className="cont-bef imgs">
                     <p>Photo before</p>
                     {imgSrc && (
@@ -47,6 +55,9 @@ const Post = () => {
                         />
                     )}
                 </div>
+            </div>
+            <div className="userfound">
+                Foydalanuvchi: {user ? user.fname : "Qidirilmoqda"}
             </div>
         </main>
     );
