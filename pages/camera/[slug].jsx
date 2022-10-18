e870c6bea2366e42c81061c290ed68c05c3cd03a
muhammad-uzbek <uzbekjon2003@gmail.com>
2022-10-18 18:45:16 +0500

    camera upd9

--- pages/camera/[slug].jsx
+++ pages/camera/[slug].jsx
@@ -1,6 +1,8 @@
 import { useRouter } from "next/router";
 import Webcam from "react-webcam";
 import React, { useState, useRef, useEffect } from "react";
+import axios from 'axios';
+import FormData from 'form-data';
 
 const Post = () => {
     const router = useRouter()
@@ -25,6 +27,26 @@ const Post = () => {
         setStep(3)
         setImgSrcAfter(imageSrc)
     }, [webcamRef, setImgSrcAfter]);
+    const upload = async () => {
+        var data = new FormData();
+        data.append('demo_image', imgSrcBefore);
+        var config = {
+        method: 'post',
+        url: 'http://164.92.248.91:3096/imageserver/image',
+        headers: { 
+            ...data.getHeaders()
+        },
+        data : data
+        };
+        axios(config)
+        .then(function (response) {
+        console.log(JSON.stringify(response.data));
+        })
+        .catch(function (error) {
+        console.log(error);
+        });
+    }
+
     useEffect(()=>{
         console.log(slug);
         fetch("/api/merch")
@@ -55,7 +77,11 @@ const Post = () => {
                     )}
                 </div>
                 <div className="cont-bef camerabtn">
-                   <button onClick={step == 2 ? capture2 : capture} className="btn">{step}</button> 
+                    {
+                        step == 3 ? <button onClick={upload}>Upload</button> : <button onClick={step == 2 ? capture2 : capture} className="btn">{step}</button> 
+
+                    }
+                   
                 </div>
                 <div className="cont-bef imgs">
                     <p>Photo after</p>
