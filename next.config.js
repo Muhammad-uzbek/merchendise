12e2835b9978d96575f32f588e10b55ecf9da4c5
Muhammadqodir Abduraxmanov <60865598+Muhammad-uzbek@users.noreply.github.com>
2022-10-23 14:37:41 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  swcMinify: true,
-  webpack5: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false };
-
-    return config;
-  },
+  swcMinify: true
 }
 
 module.exports = nextConfig
